d7d6b3006b217fab17fa9febee94c7f556b5031e
Mantis <rclifford@cybershade.org>
2024-04-11 10:46:05 +0100

    Refactored code

--- VhostScan/vhostscan.go
+++ VhostScan/vhostscan.go
@@ -18,26 +18,30 @@ import (
 )
 
 type Results struct {
-	ResponseStatus  string `json:omitempty`
-	Host            string
-	IP              string
-	Title           string
-	ResponseHeaders []string
-	ResponseBody    string
+	ResponseStatus  string   `json:"ResponseStatus,omitempty"`
+	Host            string   `json:"Host"`
+	IP              string   `json:"IP"`
+	Title           string   `json:"Title"`
+	ResponseHeaders []string `json:"ResponseHeaders"`
+	ResponseBody    string   `json:"ResponseBody"`
+}
+
+type Work struct {
+	Domain string
+	IP     string
 }
 
 var (
-	finalResults []Results
-	includeBody  bool
+	resultsChan = make(chan Results, 200)
+	includeBody bool
 )
 
 func main() {
-	// Todo: add a concurrency count
-
 	i := flag.String("i", "", "IP addresses file to read from")
 	d := flag.String("d", "", "Domain names file to read from")
 	v := flag.Bool("v", false, "Show verbose errors")
 	ib := flag.Bool("b", false, "Include the Body of the response in the output")
+	c := flag.Int("c", 10, "Number of concurrent workers")
 
 	flag.Parse()
 
@@ -59,169 +63,145 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	var resultsChan = make(chan Results, 200)
-
 	defer domains.Close()
 
-	scanner := bufio.NewScanner(domains)
+	domainList := getLines(domains)
+	ipList := getLines(ips)
 
-	var domainList []string
-	// var results []Results
-	for scanner.Scan() {
+	workChan := make(chan Work, len(domainList)*len(ipList))
 
-		domain := scanner.Text()
-		domain = strings.TrimSpace(domain)
-
-		if domain == "" {
-			continue
+	go func() {
+		for _, domain := range domainList {
+			for _, ip := range ipList {
+				workChan <- Work{Domain: domain, IP: ip}
+			}
 		}
-		domainList = append(domainList, domain)
+		close(workChan)
+	}()
+
+	var wg sync.WaitGroup
+	for i := 0; i < *c; i++ {
+		wg.Add(1)
+		go func() {
+			for work := range workChan {
+				processWork(work, *v)
+			}
+			wg.Done()
+		}()
 	}
 
+	go func() {
+		wg.Wait()
+		close(resultsChan)
+	}()
+
+	var finalResults []Results
+	for res := range resultsChan {
+		fmt.Printf("%s - %s\n", res.Host, res.IP)
+		finalResults = append(finalResults, res)
+	}
+
+	saveResults(finalResults)
+}
+
+func getLines(f *os.File) []string {
+	var lines []string
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line != "" {
+			lines = append(lines, line)
+		}
+	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+	return lines
+}
 
-	ipScanner := bufio.NewScanner(ips)
-	var ipList []string
-	for ipScanner.Scan() {
-		ip := ipScanner.Text()
-		ip = strings.TrimSpace(ip)
+func processWork(work Work, verbose bool) {
+	dialer := &net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+	}
 
-		if ip == "" {
-			continue
+	conn, err := tls.DialWithDialer(dialer, "tcp", work.IP+":443", &tls.Config{
+		InsecureSkipVerify: true,
+		ServerName:         work.Domain,
+	})
+
+	if err != nil {
+		if verbose {
+			log.Printf("Could not connect to %s: %v\n", work.IP, err)
 		}
-
-		ipList = append(ipList, ip)
+		return
 	}
-	if err := ipScanner.Err(); err != nil {
-		log.Fatal(err)
+	defer conn.Close()
+
+	http.DefaultTransport.(*http.Transport).DialContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
+		if addr == work.Domain+":443" {
+			addr = work.IP + ":443"
+		}
+		return dialer.DialContext(ctx, network, addr)
 	}
 
-	var wg sync.WaitGroup
+	httpReq, err := http.NewRequest("GET", "https://"+work.Domain+"/", nil)
+	if err != nil {
+		if verbose {
+			log.Printf("Could not create request: %v", err)
+		}
+		return
+	}
 
-	// Todo: This should be channel
-	for _, domain := range domainList {
+	httpReq.Host = work.Domain
 
-		// Todo: This should be channel
-		for _, ip := range ipList {
+	resp, err := http.DefaultClient.Do(httpReq)
+	if err != nil {
+		if verbose {
+			log.Printf("Could not perform request: %v", err)
+		}
+		return
+	}
+	defer resp.Body.Close()
 
-			dialer := &net.Dialer{
-				Timeout:   30 * time.Second,
-				KeepAlive: 30 * time.Second,
+	var body string
+	if includeBody {
+		buf := new(bytes.Buffer)
+		_, err := buf.ReadFrom(resp.Body)
+		if err != nil {
+			if verbose {
+				log.Printf("Could not read response body: %v", err)
 			}
-
-			wg.Add(1)
-			// Todo: this should be a real function
-			go func() {
-
-				defer wg.Done()
-
-				conn, err := tls.DialWithDialer(dialer, "tcp", ip+":443", &tls.Config{
-					InsecureSkipVerify: true,
-					ServerName:         domain,
-				})
-
-				if err != nil {
-					if *v {
-						log.Printf("Could not connect to %s: %v\n", ip, err)
-					}
-					return
-				}
-
-				defer conn.Close()
-
-				http.DefaultTransport.(*http.Transport).DialContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
-					if addr == domain+":443" {
-						addr = ip + ":443"
-					}
-					return dialer.DialContext(ctx, network, addr)
-				}
-
-				// Perform http request
-				httpReq, err := http.NewRequest("GET", "https://"+domain+"/", nil)
-				if err != nil {
-					if *v {
-						log.Printf("Could not create request: %v", err)
-					}
-					return
-				}
-
-				httpReq.Host = domain
-
-				resp, err := http.DefaultClient.Do(httpReq)
-				if err != nil {
-					if *v {
-						log.Printf("Could not perform request: %v", err)
-					}
-					return
-				}
-
-				defer resp.Body.Close()
-
-				var body string
-				if includeBody {
-					buf := new(bytes.Buffer)
-					_, err := buf.ReadFrom(resp.Body)
-					if err != nil {
-						if *v {
-							log.Printf("Could not read response body: %v", err)
-						}
-					}
-					body = buf.String()
-				}
-
-				res := Results{
-					ResponseStatus:  resp.Status,
-					Host:            domain,
-					IP:              ip,
-					Title:           resp.Header.Get("Title"),
-					ResponseHeaders: resp.Header["Server"],
-					ResponseBody:    body,
-				}
-
-				resultsChan <- res
-
-				finalResults = append(finalResults, Results{
-					ResponseStatus:  resp.Status,
-					Host:            domain,
-					IP:              ip,
-					Title:           resp.Header.Get("Title"),
-					ResponseHeaders: resp.Header["Server"],
-					ResponseBody:    body,
-				})
-			}()
 		}
+		body = buf.String()
 	}
 
-	for res := range resultsChan {
-		fmt.Printf("%s - %s\n", res.Host, res.IP)
+	res := Results{
+		ResponseStatus:  resp.Status,
+		Host:            work.Domain,
+		IP:              work.IP,
+		Title:           resp.Header.Get("Title"),
+		ResponseHeaders: resp.Header["Server"],
+		ResponseBody:    body,
 	}
 
-	wg.Wait()
-	close(resultsChan)
+	resultsChan <- res
+}
 
-	// Get current time
+func saveResults(results []Results) {
 	currentTime := time.Now()
-
-	// Format the current time to desired format
 	formattedTime := currentTime.Format("2006-01-02_15-04-05")
-
-	// Create a new file with the current time in the name
 	file, err := os.Create("results_" + formattedTime + ".json")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	// Convert the finalResults slice to JSON
-	data, err := json.Marshal(finalResults)
+	data, err := json.Marshal(results)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Write the JSON data to the file
 	_, err = file.Write(data)
 	if err != nil {
 		log.Fatal(err)
